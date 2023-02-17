f4d241d32869d159c1d5bbcee853d37f9902424a
Julio Cesar Marques da Rocha <cesar_mk@outlook.com>
2023-02-17 15:55:30 -0300

    Order

---
+++ src/main/java/com/cosmos/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.cosmos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cosmos.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long> {
+
+}
---
+++ src/main/java/com/cosmos/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.cosmos.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cosmos.course.entities.Order;
+import com.cosmos.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+	
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
