63a525ae76d812cb5710b85810a9bde1ad90c2c5
Julio Cesar Marques da Rocha <cesar_mk@outlook.com>
2023-02-17 14:52:55 -0300

    Service layer

---
+++ src/main/java/com/cosmos/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.cosmos.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cosmos.course.entities.User;
+import com.cosmos.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
