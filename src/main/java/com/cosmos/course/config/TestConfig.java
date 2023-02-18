10b1b8e860519313ca1d9270e8bcbad8f407d726
Julio Cesar Marques da Rocha <cesar_mk@outlook.com>
2023-02-18 15:59:11 -0300

    Payment

--- src/main/java/com/cosmos/course/config/TestConfig.java
+++ src/main/java/com/cosmos/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.cosmos.course.entities.Category;
 import com.cosmos.course.entities.Order;
 import com.cosmos.course.entities.OrderItem;
+import com.cosmos.course.entities.Payment;
 import com.cosmos.course.entities.Product;
 import com.cosmos.course.entities.User;
 import com.cosmos.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 		
 	}
 	
